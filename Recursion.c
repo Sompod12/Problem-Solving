d72bbb68e98832cdd00390946f3d6fb1dc00fdc2
Sompod12 <mitra242-35-326@diu.edu.bd>
2025-04-29 01:44:05 +0600

    Add files via upload

---
+++ Recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int sum(int n);
+int main(){
+    int num, result;
+    printf("Enter a positive number:");
+    scanf("%d", &num);
+
+    result = sum(num);
+
+    printf("Sum:%d", result);
+return 0;
+}
+int sum(int n){
+    if(n != 0){
+        return n + sum(n-1);
+    }
+    else{
+        return n;
+    }
+}
